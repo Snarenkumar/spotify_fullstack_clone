d31929b229cd8199a72d827ed796c6da66b2a5be
Snarenkumar <vssnaren@gmail.com>
2023-10-07 22:27:26 +0530

    some changes in the main file

--- home/views.py
+++ home/views.py
@@ -2,5 +2,7 @@ from django.shortcuts import render,HttpResponse
 
 # Create your views here.
 def login(request):
-    return render (request,'login.html')
+    
+
+    return render (request,'login.html',)
     
